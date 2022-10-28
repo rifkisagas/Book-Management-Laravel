f2f0ae2e39bb8006de0020101e46dea385045cba
Rifki Arisagas <Arisagasr@gmail.com>
2022-10-28 17:03:01 +0700

    Komit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,19 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1 class="m-o text-dark">Dashboard</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="car-body">
+                    <p class="mb-0">You are Logged In!</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+@stop
